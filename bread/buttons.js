b804a7876f93848e9273a36a8387a777a602e528
The-Wellerman-Group <152121645+The-Wellerman-Group@users.noreply.github.com>
2024-07-25 21:12:30 -0600

    Create buttons.js

---
+++ bread/buttons.js
@@ -0,0 +1,32 @@
+var started = true;
+
+$(document).ready(function(){
+  $("#sorry").css("display","none");
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    //Check if on mobile or not
+    //$("body").css("overflow","hidden");
+    mobile = true;
+  }
+  else {
+    mobile = false;
+    //$("#sorry").css("display","none");
+  }
+  $("#percentages")[0].checked = true;
+  $("#stats").on('change', function(){
+    if ($("#stats")[0].checked == true){
+      $("#stats_for_nerds").css("display","block");
+    }
+    else {
+      $("#stats_for_nerds").css("display","none");
+    }
+  })
+  $("#percentages").on('change', function(){
+    if ($("#percentages")[0].checked == true){
+      displayPercentages = true;
+    }
+    else {
+      displayPercentages = false;
+    }
+  })
+
+})
